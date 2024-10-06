8f2c1bc8b43744a96a00038717bb98a00b3237a2
hyoaru <jjcabreraaaa@gmail.com>
2024-10-06 19:25:56 +0800

    [add] sign up functionality

--- api/core-api/app/Http/Controllers/AuthController.php
+++ api/core-api/app/Http/Controllers/AuthController.php
@@ -2,19 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Authentication\SignUpRequest as AuthenticationSignUpRequest;
 
 class AuthController extends Controller
 {
-    public function sign_up(Request $request){
-        return 'sign-up';
+    public function sign_up(AuthenticationSignUpRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $user = User::create([
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
+        ]);
+
+        $token = $user->createToken($user->email);
+
+        $response = [
+            'data' => [
+                'user' => $user,
+                'token' => $token->plainTextToken,
+            ]
+        ];
+
+        return $response;
     }
 
-    public function sign_in(Request $request){ 
+    public function sign_in(Request $request)
+    {
         return 'sign-in';
     }
 
-    public function sign_out(Request $request){
+    public function sign_out(Request $request)
+    {
         return 'sign-out';
     }
 }
---
+++ api/core-api/app/Http/Requests/Authentication/SignUpRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Authentication;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignUpRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+}
