92c2a2b9aef819c7daddbbbeea13ac8cd3d2a75d
hyoaru <jjcabreraaaa@gmail.com>
2024-10-06 12:08:04 +0800

    [add] gunicorn

--- api/ml-api/main.py
+++ api/ml-api/main.py
@@ -1,6 +1,4 @@
 from app import create_app, initalize_nltk_resource
 
-if __name__ == '__main__':
-    initalize_nltk_resource()
-    app = create_app()
-    app.run(debug=True)
+initalize_nltk_resource()
+app = create_app()
