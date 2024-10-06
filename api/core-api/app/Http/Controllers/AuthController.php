ba9c8384e5e1cb08196601e46d3a3289b2737a61
hyoaru <jjcabreraaaa@gmail.com>
2024-10-06 19:51:07 +0800

    [add] sign in functionality

--- api/core-api/app/Http/Controllers/AuthController.php
+++ api/core-api/app/Http/Controllers/AuthController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Requests\Authentication\SignUpRequest as AuthenticationSignUpRequest;
+use App\Http\Requests\Authentication\SignInRequest;
+use App\Http\Requests\Authentication\SignUpRequest;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function sign_up(AuthenticationSignUpRequest $request)
+    public function sign_up(SignUpRequest $request)
     {
         $validatedData = $request->validated();
 
@@ -16,7 +18,7 @@ class AuthController extends Controller
             'first_name' => $validatedData['first_name'],
             'last_name' => $validatedData['last_name'],
             'email' => $validatedData['email'],
-            'password' => $validatedData['password'],
+            'password' => bcrypt($validatedData['password']),
         ]);
 
         $token = $user->createToken($user->email);
@@ -31,9 +33,29 @@ class AuthController extends Controller
         return $response;
     }
 
-    public function sign_in(Request $request)
+    public function sign_in(SignInRequest $request)
     {
-        return 'sign-in';
+        $validatedData = $request->validated();
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+            return [
+                'message' => 'The provided credentials are incorrect',
+                'errors' => []
+            ];
+        }
+
+        $token = $user->createToken($user->email);
+
+        $response = [
+            'data' => [
+                'user' => $user,
+                'token' => $token->plainTextToken,
+            ]
+        ];
+
+        return $response;
     }
 
     public function sign_out(Request $request)
