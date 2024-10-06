65b9222ff12097ab3f40e02b0a90f33d732c2b8d
hyoaru <jjcabreraaaa@gmail.com>
2024-10-06 22:01:51 +0800

    [fix] migration not rolling back

--- api/core-api/database/migrations/2024_10_06_104746_update_users_table_add_first_last_name.php
+++ api/core-api/database/migrations/2024_10_06_104746_update_users_table_add_first_last_name.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,8 +13,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('first_name')->after('id');
-            $table->string('last_name')->after('first_name');
+            $table->string('first_name')->nullable()->after('id');
+            $table->string('last_name')->nullable()->after('first_name');
+        });
+
+        DB::table('users')->update([
+            'first_name' => DB::raw('SUBSTR(name, 1, INSTR(name, " ") - 1)'), 
+            'last_name' => DB::raw('SUBSTR(name, INSTR(name, " ") + 1)') 
+        ]);
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->nullable(false)->change();
+            $table->string('last_name')->nullable(false)->change();
 
             $table->dropColumn('name');
         });
@@ -25,8 +36,15 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name')->after('id');
+            $table->string('name')->nullable()->after('id');
+        });
 
+        DB::table('users')->update([
+            'name' => DB::raw('first_name || " " || last_name')
+        ]);
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->nullable(false)->change();
             $table->dropColumn(['first_name', 'last_name']);
         });
     }
