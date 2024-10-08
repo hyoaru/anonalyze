6e4a549de8c2339f17254c878f369da08881cec0
hyoaru <jjcabreraaaa@gmail.com>
2024-10-09 02:59:43 +0800

    [add] relation to thread analytic

--- api/core-api/app/Models/ThreadExtractedConceptGroup.php
+++ api/core-api/app/Models/ThreadExtractedConceptGroup.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ThreadExtractedConceptGroup extends Model
@@ -14,5 +15,9 @@ class ThreadExtractedConceptGroup extends Model
 
     public function threadExtractedConcepts(): HasMany {
         return $this->hasMany(ThreadExtractedConcept::class);
+    }
+
+    public function threadAnalytic(): BelongsTo {
+        return $this->belongsTo(ThreadAnalytic::class);
     }
 }
