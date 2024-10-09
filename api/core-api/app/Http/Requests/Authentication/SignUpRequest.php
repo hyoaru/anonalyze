2b12801809c803bf798fa1c9b19ef9aa370e7437
hyoaru <jjcabreraaaa@gmail.com>
2024-10-09 21:40:21 +0800

    [update] sign up request validation

--- api/core-api/app/Http/Requests/Authentication/SignUpRequest.php
+++ api/core-api/app/Http/Requests/Authentication/SignUpRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Authentication;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class SignUpRequest extends FormRequest
 {
@@ -14,10 +15,15 @@ class SignUpRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'first_name' => 'required|string|max:256',
+            'last_name' => 'required|string|max:256',
+            'email' => 'required|string|email|max:256|unique:users',
+            'password' => [
+                'required', 
+                'string', 
+                Password::min(8)->max(256)->numbers()->mixedCase(),
+                'confirmed'
+            ],
         ];
     }
 }
