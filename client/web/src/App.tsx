887cdaac9712d5807686db5c92fa9f882b7c5a36
hyoaru <jjcabreraaaa@gmail.com>
2024-10-23 12:03:32 +0800

    [add] tanstack router

--- client/web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Button } from "@/components/ui/button"
-
-export default function App() {
-  return (
-    <div><Button variant={'secondary'}>Button</Button></div>
-  )
-}
