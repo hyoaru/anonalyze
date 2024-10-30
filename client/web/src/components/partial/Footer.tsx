4742f4bddf948c87ffda4fd2d1198fdf4ad1d70a
hyoaru <jjcabreraaaa@gmail.com>
2024-10-30 16:27:47 +0800

    [add] footer responsivity

--- client/web/src/components/partial/Footer.tsx
+++ client/web/src/components/partial/Footer.tsx
@@ -6,41 +6,29 @@ import { Link } from "@tanstack/react-router";
 export default function Footer() {
   return (
     <footer className="pb-6 backdrop-blur-sm">
-      <BaseContainer className="flex flex-col items-center md:flex-row">
+      <BaseContainer className="flex flex-col items-center text-sm sm:text-base md:flex-row">
         <div className="flex items-center gap-2 font-custom font-medium uppercase">
           <Brackets strokeWidth={2.6} size={24} className="text-main-accent" />
-          <p>
+          <p className="">
             <span className="font-sans font-bold">©</span>
-            <span>{" 2024 "}</span>
-            <Button
-              variant={"link"}
-              asChild
-              className="p-0 text-base font-medium"
-            >
+            <span className="">{" 2024 "}</span>
+            <Button variant={"link"} asChild className="p-0 font-medium text-base">
               <a href="https://github.com/hyoaru">
                 <span>hyoaru</span>
               </a>
             </Button>
           </p>
           <hr className="w-8 border-foreground/20" />
-          <span>made with tears</span>
+          <span className="">made with tears</span>
         </div>
         <div className="flex items-center gap-2 font-custom font-medium uppercase md:ms-auto">
-          <Button
-            variant={"link"}
-            asChild
-            className="px-0 text-base font-medium"
-          >
+          <Button variant={"link"} asChild className="px-0 font-medium text-base">
             <Link to="/about">
               <p>about</p>
             </Link>
           </Button>
           <hr className="w-8 border-foreground/20" />
-          <Button
-            variant={"link"}
-            asChild
-            className="px-0 text-base font-medium"
-          >
+          <Button variant={"link"} asChild className="px-0 font-medium text-base">
             <a href="https://github.com/hyoaru/anonalyze">
               <p>github</p>
             </a>
