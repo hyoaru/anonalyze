a9290e688c93035cb3ed52f08ecdf4ec7871d09c
hyoaru <jjcabreraaaa@gmail.com>
2024-11-01 18:11:26 +0800

    [fix] error component layout

--- client/web/src/components/defaults/ErrorComponent.tsx
+++ client/web/src/components/defaults/ErrorComponent.tsx
@@ -4,12 +4,22 @@ import { Button } from "@/components/ui/button";
 export default function ErrorComponent({ error }: { error: Error }) {
   const router = useRouter();
   return (
-    <div className="flex inset-0 flex-col items-center justify-center">
-      <div className="items-center-justify-center flex flex-col gap-1 text-center mb-10">
+    <div className="absolute inset-0 flex flex-col items-center justify-center">
+      <div className="items-center-justify-center mb-10 flex flex-col gap-1 text-center">
         <p className="text-3xl font-semibold">An error has occured</p>
         <p className="text-muted-foreground">{error?.message}</p>
       </div>
-      <Button onClick={() => router.invalidate()}>Revalidate</Button>
+      <div className="flex gap-2">
+        <Button onClick={() => router.invalidate()}>Revalidate</Button>
+        <Button
+          onClick={() => {
+            router.invalidate();
+            router.navigate({ to: "/dashboard" });
+          }}
+        >
+          Go back home
+        </Button>
+      </div>
     </div>
   );
 }
