6ab3b0bbc0e9c234d8dae81b721f703c3d73bf49
hyoaru <jjcabreraaaa@gmail.com>
2024-11-02 14:47:52 +0800

    [update] rake min to 1 and max to 3

--- api/ml-api/app/api/namespaces/concept.py
+++ api/ml-api/app/api/namespaces/concept.py
@@ -19,7 +19,7 @@ class ExtractConcept(Resource):
         data = parser.parse_args()
         texts = data['texts'][0]
 
-        r = Rake(min_length=2, max_length=4)
+        r = Rake(min_length=1, max_length=3)
         r.extract_keywords_from_sentences(texts)
         phrase_scores = r.get_ranked_phrases_with_scores()
         phrase_scores_map = {scored_phrase[1]:scored_phrase[0] for scored_phrase in phrase_scores}
